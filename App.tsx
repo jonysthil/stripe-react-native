42047e2467ece046b4e61d31f8994361f23ab716
Jonathan JG <jonysthil@gmail.com>
2022-07-14 22:13:36 -0500

    implementando libreria stripe y mostrando formulario basico

--- App.tsx
+++ App.tsx
@@ -1,117 +1,51 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * Generated with the TypeScript template
- * https://github.com/react-native-community/react-native-template-typescript
- *
- * @format
- */
-
-import React, {type PropsWithChildren} from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
-
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
-
-const Section: React.FC<
-  PropsWithChildren<{
-    title: string;
-  }>
-> = ({children, title}) => {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-};
+import { StripeProvider } from '@stripe/stripe-react-native'
+import { CardField, useStripe } from '@stripe/stripe-react-native';
+import React from 'react'
+import { Button, StyleSheet, Text } from 'react-native'
 
 const App = () => {
-  const isDarkMode = useColorScheme() === 'dark';
+	return (
+		<StripeProvider
+			publishableKey="pk_test_5a9oB6mjHXvqVtmhSszb3vqw00VBm5flPt"
+		>
+			<PaymentScreen />
+		</StripeProvider>
+	)
+}
 
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
+const PaymentScreen = () => {
+	const { confirmPayment } = useStripe();
+	console.log(confirmPayment)
+	return(
+		<>
+			<CardField
+				postalCodeEnabled={false}
+				placeholders={{
+				number: '4242 4242 4242 4242',
+				}}
+				cardStyle={{
+				backgroundColor: '#FFFFFF',
+				textColor: '#000000',
+				}}
+				style={{
+				width: '100%',
+				height: 50,
+				marginVertical: 30,
+				}}
+				onCardChange={(cardDetails) => {
+				console.log('cardDetails', cardDetails);
+				}}
+				onFocus={(focusedField) => {
+				console.log('focusField', focusedField);
+				}}
+			/>
+			<Button title='Pagar' onPress={() => confirmPayment} />
+		</>
+	)
+} 
 
-  return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
-    </SafeAreaView>
-  );
-};
+const style = StyleSheet.create({
 
-const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-});
+}) 
 
-export default App;
+export default App
